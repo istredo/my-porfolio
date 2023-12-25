e394cd61ab9d47989fbe1845a0f62ef6e32a0c64
istredo <uvarov6799@gmail.com>
2023-12-25 17:33:41 +0300

    feat: CV added,  photo to black theme

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -20,7 +20,7 @@ const Content = () => {
 					<div className="right">
 						<h4 className='descVisible'>Are you looking for developer to your projects?<br />You can rely on me!</h4>
 
-						<a className='download__CV' href="#anchor" target="_blank" rel="noopener noreferrer">
+						<a className='download__CV' href="/cv.pdf" target="_blank" rel="noopener noreferrer" download>
 							Download CV
 							<svg className='download__svg' xmlns="http://www.w3.org/2000/svg" width='40' fill="none" viewBox="0 0 24 24">
 								<path stroke="#009b77" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" d="M21 21H3m15-10-6 6m0 0-6-6m6 6V3" />
