e13c05b3fb63714f8c3799831ef9d1115fdceb66
istredo <uvarov6799@gmail.com>
2023-12-31 13:47:31 +0300

    fix: url adress in projects

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -10,7 +10,7 @@ const Modal = ({ open, setOpen }) => {
 	const modalList = [
 		{ id: 0, title: "allods", description: "Тестовое задание в astrum entertaiment", url: "https://allods.vercel.app/", img: '/img/allods.png' },
 		{ id: 1, title: "shoes", description: "Магазин обуви. React, Axios, SCSS ", url: "", img: '/img/shoes.png' },
-		{ id: 2, title: "pizza", description: "Крупное приложение по заказу пиццы. React, Axios, SCSS, Redux, Адаптивная верстка.", url: "my-pizza-istredo.vercel.app", img: '/img/pizza.png' },
+		{ id: 2, title: "pizza", description: "Крупное приложение по заказу пиццы. React, Axios, SCSS, Redux, Адаптивная верстка.", url: "https://my-pizza-istredo.vercel.app", img: '/img/pizza.png' },
 		{ id: 3, title: "hotel", description: "Сайт-визитка отеля. С возможностью бронирования номеров. Вся информация о клиенте передается через телеграм-бота.", url: "https://hotel-app-mauve.vercel.app/", img: '/img/hotel.png' },
 		{ id: 4, title: "parallax", description: "Верстка сайта с эффектом паралакс", url: "https://istredo.github.io/Magic-forest-parallax-/", img: '/img/forest.png' },
 		{ id: 5, title: "gallery", description: "Шаблон галлереи с интересной механикой", url: "https://gallery-istredo.vercel.app", img: '/img/gallery.png' },
