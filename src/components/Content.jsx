dcdd56c60b89e50319196eb46fd18b9f4bb5ea38
istredo <uvarov6799@gmail.com>
2024-03-24 21:02:03 +0300

    fix: mobile styles

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -6,24 +6,26 @@ const Content = () => {
 			<div className="container">
 				<div className="content__container">
 					<div className="left">
-						<h2>Hi,<br /> I'm <span>Andrey</span></h2>
-						<h3>Front-end developer</h3>
-						<a className='button__contact' href="https://t.me/istredo" target="_blank" rel="noopener noreferrer">
+						<div className="left__top">
+							<h2>Hi, I'm <span>Andrey</span></h2>
+							<h3>Front-end developer</h3>
+						</div>
+						<button className='button__contact' href="https://t.me/istredo" target="_blank" rel="noopener noreferrer">
 							Contact me
 							<span className='button__line button__line--top'></span>
 							<span className='button__line button__line--right'></span>
 							<span className='button__line button__line--bottom'></span>
 							<span className='button__line button__line--left'></span>
-						</a>
+						</button>
 					</div>
 					<div className="right">
 						<h4 className='descVisible'>Are you looking for developer to your projects?<br />You can rely on me!</h4>
-						<a className='download__CV' href="/Uvarov_Andrey_Frotend.pdf" target="_blank" rel="noopener noreferrer" download>
+						<button className='download__CV' href="/Uvarov_Andrey_Frotend.pdf" target="_blank" rel="noopener noreferrer" download>
 							Download CV
 							<svg className='download__svg' xmlns="http://www.w3.org/2000/svg" width='40' fill="none" viewBox="0 0 24 24">
 								<path stroke="#009b77" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" d="M21 21H3m15-10-6 6m0 0-6-6m6 6V3" />
 							</svg>
-						</a>
+						</button>
 					</div>
 				</div>
 			</div>
