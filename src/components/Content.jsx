8ae91f5bf3c0a11713a670883e6c88b7f757804b
istredo <uvarov6799@gmail.com>
2024-03-25 17:27:57 +0300

    feat: added styles for mobile version

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
 import { useLang } from '../utils/useLang'
+import { getWindowWidth } from '../utils/windowWidth'
 const Content = () => {
 	const { lang, translations } = useLang()
+	const contactBtn = () => {
+		setTimeout(() => window.location = 'https://t.me/istredo', 1500);
+	}
+	const downloaBbtn = () => {
+		setTimeout(() => window.location.href = '/Uvarov_Andrey_Frotend.pdf', 1500);
+	}
+	const { windowWidth } = getWindowWidth()
 	return (
 		<section className='content'>
 			<div className="container">
@@ -11,23 +19,42 @@ const Content = () => {
 							<h2>{translations[lang].main_page.content_hi}, {translations[lang].main_page.content_i} <span>{translations[lang].main_page.content_name}</span></h2>
 							<h3>{translations[lang].main_page.content_fe}</h3>
 						</div>
-						<a className='button__contact' href="https://t.me/istredo" target="_blank" rel="noopener noreferrer">
-							{translations[lang].btn.contact}
-							<span className='button__line button__line--top'></span>
-							<span className='button__line button__line--right'></span>
-							<span className='button__line button__line--bottom'></span>
-							<span className='button__line button__line--left'></span>
-						</a>
+						{
+							windowWidth <= 600
+								? <a className='button__contact' target="_blank" rel="noopener noreferrer" onClick={() => contactBtn()} draggable='false'>
+									{translations[lang].btn.contact}
+									<span className='button__line button__line--top'></span>
+									<span className='button__line button__line--right'></span>
+									<span className='button__line button__line--bottom'></span>
+									<span className='button__line button__line--left'></span>
+								</a>
+								: <a className='button__contact' href='https://t.me/istredo' target="_blank" rel="noopener noreferrer">
+									{translations[lang].btn.contact}
+									<span className='button__line button__line--top'></span>
+									<span className='button__line button__line--right'></span>
+									<span className='button__line button__line--bottom'></span>
+									<span className='button__line button__line--left'></span>
+								</a>
+						}
 					</div>
 					<div className="right">
 						<h4 className='descVisible'>{translations[lang].main_page.content_info}<br />{translations[lang].main_page.content_rely}
 						</h4>
-						<a className='download__CV' href="/Uvarov_Andrey_Frotend.pdf" target="_blank" rel="noopener noreferrer" download>
-							{translations[lang].btn.cv}
-							<svg className='download__svg' xmlns="http://www.w3.org/2000/svg" width='40' fill="none" viewBox="0 0 24 24">
-								<path stroke="#009b77" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" d="M21 21H3m15-10-6 6m0 0-6-6m6 6V3" />
-							</svg>
-						</a>
+						{
+							windowWidth <= 600
+								? <a className='download__CV' target="_blank" rel="noopener noreferrer" download onClick={() => downloaBbtn()} draggable='false'>
+									{translations[lang].btn.cv}
+									<svg className='download__svg' xmlns="http://www.w3.org/2000/svg" width='40' fill="none" viewBox="0 0 24 24">
+										<path stroke="#009b77" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" d="M21 21H3m15-10-6 6m0 0-6-6m6 6V3" />
+									</svg>
+								</a>
+								: <a className='download__CV' href="/Uvarov_Andrey_Frotend.pdf" target="_blank" rel="noopener noreferrer" download>
+									{translations[lang].btn.cv}
+									<svg className='download__svg' xmlns="http://www.w3.org/2000/svg" width='40' fill="none" viewBox="0 0 24 24">
+										<path stroke="#009b77" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" d="M21 21H3m15-10-6 6m0 0-6-6m6 6V3" />
+									</svg>
+								</a>
+						}
 					</div>
 				</div>
 			</div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,7 +14,7 @@ const Header = () => {
 				<div className="header__container">
 					<div className="logo__container">
 						<svg className='logo' xmlns="http://www.w3.org/2000/svg" height='50' viewBox="0 -2 30 30"><path fill={theme === 'light' ? '#232323' : '#dadee1'} fillRule="evenodd" d="m29.717 12.531-5.991-4.843c-.394-.384-1.033-.384-1.426 0-.394.384-.394 1.006 0 1.391l5.191 4.197-5.191 4.198a.965.965 0 0 0 0 1.39c.393.385 1.032.385 1.426 0l5.991-4.843c.21-.205.299-.477.285-.745.014-.268-.075-.541-.285-.745ZM7.7 17.473l-5.191-4.197L7.7 9.079c.394-.385.394-1.008 0-1.391-.393-.385-1.032-.385-1.426 0L.283 12.531c-.21.204-.299.477-.285.745-.014.268.075.54.285.745l5.991 4.843c.394.385 1.033.385 1.426 0a.9662.9662 0 0 0 0-1.391ZM21.96.123c-.479-.271-1.09-.11-1.366.358l-12.5 24.087c-.277.469-.162 1.186.366 1.339.645.187 1.09.111 1.366-.358l12.5-24.088c.276-.469.112-1.067-.366-1.338Z" /></svg>
-						<button className='btn__language' onClick={() => dispatch(toggleLang())}>{translations[lang].header.version}</button>
+						<button className='btn__language' onClick={() => dispatch(toggleLang())} draggable='false'>{translations[lang].header.version}</button>
 					</div>
 					<a href="mailto:contact@istredo.fun" className="mail" style={{ color: `${theme === 'light' ? '#232323' : '#dadee1'}` }}>contact@istredo.fun</a>
 					<div className="nav" onClick={() => dispatch(modalOpen())}>{translations[lang].header.works}</div>
